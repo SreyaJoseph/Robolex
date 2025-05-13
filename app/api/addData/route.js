06ddada0ab88a0540b68e05c4a3a03aff829427d
SreyaJoseph <sreyajoseph2004@gmail.com>
2025-05-13 23:20:49 +0530

    Add MongoDB name and collection

--- app/api/addData/route.js
+++ app/api/addData/route.js
@@ -3,8 +3,8 @@ import clientPromise from "@/lib/mongodb";
 export async function POST(req) {
   try {
     const client = await clientPromise;
-    const db = client.db("your_database_name"); // Change this
-    const collection = db.collection("your_collection_name"); // Change this
+    const db = client.db("mydatabase"); 
+    const collection = db.collection("users"); 
 
     const data = await req.json(); // Extract data from request
     const result = await collection.insertOne(data);
