708f75efca2c2d5c21b339fd9c5e5591a1ee5956
SreyaJoseph <sreyajoseph2004@gmail.com>
2025-05-14 11:32:19 +0530

    removed one option in the mongodb constructor

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,9 +3,6 @@ import { MongoClient } from 'mongodb';
 const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // only for debugging—allows you to bypass strict cert validation
-  // remove this in production!
-  tlsAllowInvalidCertificates: true,
 });
 
 let clientPromise;
